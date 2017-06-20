d4cc77324519c6b626be61a83fa1218411104d0a
Lusinia <Limonchik17@yandex.ru>
2017-06-20 16:07:15 +0300

    commit for remote

--- js/mainTanks.js
+++ js/mainTanks.js
@@ -2,9 +2,9 @@
  * Created by alenka on 20.06.17.
  */
 function Ruler(num){
-    var id = Math.random();
-
-    this.add = function(num){
+    this.number  = num;
+ var that = this;
+    this.add = function(){
       var left =   num == 1 ? "205px" : "400px";
       var back =   num == 1 ? "./img/main1.png" : "./img/main2.png";
         var tank = document.createElement('div');
@@ -12,7 +12,7 @@ function Ruler(num){
             'top': '460px',
             'left': left,
             'zIndex': '10000',
-            'transition': 'all 0.12s linear',
+            'transition': 'all 0.2s linear',
             'position': 'absolute',
             'background': 'url('+ back+')',
             'backgroundSize': 'cover',
@@ -24,29 +24,29 @@ function Ruler(num){
         }
 
         tank.classList.add('ruler');
-         tank.setAttribute('data-id', id);
+         tank.setAttribute('data-number', that.number);
 
 
         $('#field').append(tank);
     };
 
-this.move = function(num) {
-    var el = $('body').find("div[data-id='" + id + "']")[0];
-    console.log('el ',el)
-    document.body.onkeydown = function (e) {
+this.move = function() {
+    var el = $('body').find("div[data-number='" + that.number + "']")[0];
+     document.body.onkeydown = function (e) {
 
         var KEYCODE_LEFT,
             KEYCODE_RIGHT,
             KEYCODE_UP,
             KEYCODE_DOWN ;
-        if (num == 1){
-            // управляем буквами
+
+        if (that.number == 1){
+            // управляем стрелками
             KEYCODE_LEFT = 37;
               KEYCODE_RIGHT = 39;
              KEYCODE_UP = 38;
              KEYCODE_DOWN = 40;
-        } else if (num == 2) {
-            // управляем  стрелками
+        } else {
+            // управляем   буквами
             KEYCODE_LEFT = 65;
             KEYCODE_RIGHT = 68;
             KEYCODE_UP = 87;
@@ -76,49 +76,41 @@ this.move = function(num) {
         (function movePlayer() {
 
             if (e.keyCode == KEYCODE_LEFT) {
-                el.style.left = (parseInt(el.style.left) - 10) + 'px';
                 direction = 'left';
                 rotate(direction);
+                el.style.left = (parseInt(el.style.left) - 10) + 'px';
+
             } else if (e.keyCode == KEYCODE_RIGHT) {
-                el.style.left = (parseInt(el.style.left) + 10) + 'px';
                 direction = 'right';
                 rotate(direction);
+                el.style.left = (parseInt(el.style.left) + 10) + 'px';
 
             } else if (e.keyCode == KEYCODE_UP) {
-                el.style.top = (parseInt(el.style.top) - 10) + 'px';
                 direction = 'top';
                 rotate(direction);
+                el.style.top = (parseInt(el.style.top) - 10) + 'px';
+
 
             } else if (e.keyCode == KEYCODE_DOWN) {
-                el.style.top = (parseInt(el.style.top) + 10) + 'px';
                 direction = 'bottom';
                 rotate(direction);
-
+                el.style.top = (parseInt(el.style.top) + 10) + 'px';
 
             }
-            // if (el.offsetTop < 0) {
-            //     el.style.top = '0px';
-            // } else if (el.offsetTop > maxHeight) {
-            //     el.style.top = maxHeight - 20 + 'px';
-            // } else if (el.offsetLeft < 0) {
-            //     el.style.left = '0px';
-            // } else if (el.offsetLeft > maxWidth) {
-            //     el.style.left = maxWidth - 10 + 'px';
-            // }
+
         })()
 
     };
 };
-this.create = function(num){
-    var tank = new Ruler();
-    tank1.add(num);
+this.create = function( ){
+    var tank = new Ruler(num);
+    tank.add();
+    tank.move();
 
 }}
-var tank1 = new Ruler();
-tank1.add(1);
-tank1.move();
+var tank1 = new Ruler(1);
+tank1.create();
 
-var tank2 = new Ruler();
-tank2.add(2);
-tank2.move();
+var tank2 = new Ruler(2);
+tank2.create();
 
